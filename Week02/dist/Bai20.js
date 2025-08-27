9a48bd6875fc8422fd77d1bf6978e7780b3b0134
MrTroll28 <{nguyenoke123@gmail.com}>
2025-08-27 08:49:41 +0700

    Bai20

---
+++ Week02/dist/Bai20.js
@@ -0,0 +1,15 @@
+"use strict";
+// Add a timeout: if the API call takes more than 2 seconds, throw an error.
+async function fetchUser(id) {
+    try {
+        await new Promise((resolve, reject) => {
+            setTimeout(() => reject(new Error("Timeout")), 2000);
+            setTimeout(() => resolve({ id, name: "User " + id }), 4000);
+        });
+        return { id, name: "User " + id };
+    }
+    catch (error) {
+        throw new Error("Request timed out");
+    }
+}
+fetchUser(1).then((user) => console.log(user)).catch((error) => console.error(error.message));
